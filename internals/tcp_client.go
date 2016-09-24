8e22b5d57e243367a64a219b1a40a1bb39714d48
Debarshi Basak <d.basak@sensysgatso.com>
2016-09-24 14:23:21 +0200

    Kinda works

--- internals/tcp_client.go
+++ internals/tcp_client.go
@@ -2,18 +2,24 @@ package pipie
 
 import (
 	"bufio"
+	"encoding/json"
+	"github.com/boltdb/bolt"
 	"log"
 	"net"
-	"net/http"
 	"strconv"
-	"github.com/debarshri/pipie/internals/ds"
 )
 
 type OnMessageFunc func(string)
 
+type MqClient struct {
+	Hostname string
+	HostPort int
+	DB       *bolt.DB
+}
 
+func (m MqClient) Receive(process OnMessageFunc) {
 
-func (m ds.MqClient) Receive(process OnMessageFunc) {
+	// Ensure all routines finish before returning
 
 	var count = 0
 	conn, err := net.Dial("tcp", m.Hostname+":"+strconv.Itoa(m.HostPort))
@@ -27,16 +33,28 @@ func (m ds.MqClient) Receive(process OnMessageFunc) {
 		if conn != nil {
 			r := bufio.NewReader(conn)
 
-			w := bufio.NewWriter(conn)
-
-			message, err := r.ReadString('\n')
+			unserialized_message, err := r.ReadString('\n')
 
 			if err != nil {
 				conn, err = net.Dial("tcp", m.Hostname+":"+strconv.Itoa(m.HostPort))
-				w.WriteString("ack")
+			} else if unserialized_message != "" {
 
-			} else if message != "" {
-				process(message)
+				var message = Message{}
+
+				b := []byte(unserialized_message)
+				err := json.Unmarshal(b, &message)
+
+				ack := Message{Key: message.Key, Value: "ack"}
+
+				d, err := json.Marshal(ack)
+
+				if err != nil {
+					log.Print(err)
+				}
+
+				conn.Write([]byte(string(d) + "\n"))
+
+				process(message.Value)
 			}
 		}
 
@@ -51,9 +69,60 @@ func (m ds.MqClient) Receive(process OnMessageFunc) {
 	}
 }
 
-func (m ds.MqClient) ReceiveFromEternity(process OnMessageFunc) {
+func (m MqClient) ReceiveAndSendAck(mq MqServer, process OnMessageFunc) {
 
-	go http.Get("http://"+m.Hostname+":"+strconv.Itoa(m.HostPort+1)+"/all")
-	m.Receive(process)
+	// Ensure all routines finish before returning
 
+	var count = 0
+	conn, err := net.Dial("tcp", m.Hostname+":"+strconv.Itoa(m.HostPort))
+
+	if err != nil {
+		log.Println(err)
+	}
+	for {
+		count++
+
+		if conn != nil {
+			r := bufio.NewReader(conn)
+
+			unserialized_message, err := r.ReadString('\n')
+
+			if err != nil {
+				conn, err = net.Dial("tcp", m.Hostname+":"+strconv.Itoa(m.HostPort))
+			} else if unserialized_message != "" {
+
+				var message = Message{}
+
+				b := []byte(unserialized_message)
+				err := json.Unmarshal(b, &message)
+
+				if err != nil {
+					log.Print(err)
+				}
+
+				process(message.Value)
+
+				log.Println("Sending ack ", message.Key)
+
+				go func(key string) {
+					mq.Send(key)
+				}(message.Key)
+
+			}
+		}
+
+		if count%1000000000 == 0 {
+			log.Println("Recaliberate the connection")
+			conn, err = net.Dial("tcp", m.Hostname+":"+strconv.Itoa(m.HostPort))
+
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
+
+func (m MqClient) ReceiveFromEternity(mq MqServer, process OnMessageFunc) {
+
+	m.ReceiveAndSendAck(mq, process)
 }
--- internals/tcp_server.go
+++ internals/tcp_server.go
@@ -9,22 +9,11 @@ import (
 	"os"
 	"strconv"
 	"time"
+	"encoding/json"
+	"sync"
 )
 
-type MqServer struct {
-	listener  net.Listener
-	queuesize int
-	ttl       int64
-}
-
-type Message struct {
-	Key string
-	Value string
-}
-
-var max_poolsize int = 10
-var curr_poolsize int = 0
-var queue []string = make([]string, 0)
+var wg sync.WaitGroup
 
 func Start() MqServer {
 	fmt.Println("Launching server...")
@@ -33,18 +22,43 @@ func Start() MqServer {
 	ln, _ := net.Listen("tcp", ":8080")
 
 	// accept connection on port
+	conn , _ := ln.Accept()
 
-	return MqServer{listener: ln}
+	return MqServer{conn: conn}
 
 }
 
 func ServerStreamAtPort(port int) MqServer {
-	fmt.Println("Starting producer...")
+	fmt.Println("Starting producer...", port)
 
 	ln, _ := net.Listen("tcp", ":"+strconv.Itoa(port))
 
-	mq := MqServer{listener: ln}
-	go mq.startService(strconv.Itoa(port + 1))
+	db, err := bolt.Open("pipie.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn , _ := ln.Accept()
+
+	mq := MqServer{conn: conn, DB:db}
+
+	//go mq.startService(strconv.Itoa(port + 1))
+
+	return mq
+}
+
+func ServerStreamAtPortWithDBLoc(port int, dblocation string) MqServer {
+	fmt.Println("Starting producer...", port)
+
+	ln, _ := net.Listen("tcp", ":"+strconv.Itoa(port))
+
+	db, err := bolt.Open(dblocation, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn , _ := ln.Accept()
+	mq := MqServer{conn: conn, DB:db}
+
+	//go mq.startService(strconv.Itoa(port + 1))
 
 	return mq
 
@@ -71,20 +85,13 @@ func (mq MqServer) startService(port string) {
 }
 
 func (mq MqServer) update() {
-	db, err := bolt.Open("pipie.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 
-	err = db.View(func(tx *bolt.Tx) error {
+	mq.DB.Update(func(tx *bolt.Tx) error {
 
 		c := tx.Bucket([]byte("data"))
 
 		if c != nil {
-			if err != nil {
-				log.Fatal("Doesnt work")
-			}
+
 			cursor := c.Cursor()
 
 			for k, v := cursor.First(); k != nil; k, v = cursor.Next() {
@@ -96,69 +103,99 @@ func (mq MqServer) update() {
 
 		return nil
 	})
+
 }
 
-func (m MqServer) send(key string, data string) {
-	con, _ := m.listener.Accept()
 
-	_, err := con.Write([]byte(data + "\n"))
+func (m MqServer) send(key string, data string) {
+
+	c := make(chan bool)
+
+	con := m.conn
+
+	message := Message{Key:key, Value:data}
+
+	serialzed_message, err := json.Marshal(message)
+
+	log.Print("Sending key", key)
+
+	_, err = con.Write([]byte(string(serialzed_message) + "\n"))
 
 	if err != nil {
 		log.Println(err)
 	}
 
-	con.Close()
+	c <- true
+
+
 }
 
 func (m MqServer) Send(data string) {
-	key := strconv.FormatInt(time.Now().UnixNano(), 10)
 
+	key := strconv.FormatInt(time.Now().UnixNano(), 10)
 	m.send(key,data)
 }
 
-//func (m MqServer) BufferedSend(data string) {
-//
-//	log.Println("Send size ", len(queue))
-//
-//	if len(queue) < 100000{
-//		queue = append(queue, data)
-//	} else {
-//		queue = queue[1:]
-//		queue = append(queue, data)
-//	}
-//
-//	curr_poolsize++
-//
-//	if curr_poolsize < max_poolsize {
-//		go m.Send(data)
-//		curr_poolsize = curr_poolsize - 1
-//	}
-//}
+func (m MqServer) DeleteKey(data string) {
 
-func (m MqServer) PersistedSend(data string) {
-
-	db, err := bolt.Open("pipie.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 	key := strconv.FormatInt(time.Now().UnixNano(), 10)
+	m.DB.Update(func(tx *bolt.Tx) error {
 
-	err = db.Update(func(tx *bolt.Tx) error {
 		c, _ := tx.CreateBucketIfNotExists([]byte("data"))
 
-		log.Println("Adding data")
-		c.Put([]byte(key), []byte(data))
+		err := c.Delete([]byte(data))
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		log.Println("Deleting key", key)
+
 		return nil
 	})
 
-	log.Println("Send size ", len(queue))
+		m.DB.Update(func(tx *bolt.Tx) error {
 
-	curr_poolsize = curr_poolsize + 1
+			c := tx.Bucket([]byte("data"))
 
-	go m.send(key, data)
+			if c != nil {
+
+				cursor := c.Cursor()
+
+				for k, v := cursor.First(); k != nil; k, v = cursor.Next() {
+					log.Println("Working for the key ", string(k))
+
+					go func(key string, value string) {
+						m.send(key,value)
+					}(string(k),string(v))
+				}
+			}
+
+			return nil
+		})
+
+
 }
 
-func (m MqServer) Stop() {
-	m.listener.Close()
+func (m MqServer) PersistedSend(data string) {
+
+	key := strconv.FormatInt(time.Now().UnixNano(), 10)
+	m.DB.Update(func(tx *bolt.Tx) error {
+
+		c, _ := tx.CreateBucketIfNotExists([]byte("data"))
+
+		log.Println("Persisting data with key", key)
+		c.Put([]byte(key), []byte(data))
+
+		return nil
+	})
+
+
+	go func(key string, value string){
+
+		 m.send(key, value)
+
+	 } (key,data)
+
 }
+
